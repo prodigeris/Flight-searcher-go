4d06811000619791a153d5fa8fe9e730117a29cb
prodigeris <arnas.kazlauskas@kilo.health>
2023-10-02 13:19:19 +0300

    feat: move api urls to env

--- components/web/main.go
+++ components/web/main.go
@@ -1,11 +1,32 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+	"os"
+)
+
+type Config struct {
+	ItineraryAnalyzerHost string
+	FlightsCollectorHost  string
+}
 
 func main() {
-	fs := http.FileServer(http.Dir("components/web/static"))
-
-	http.Handle("/", fs)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		config := Config{
+			ItineraryAnalyzerHost: os.Getenv("ITINERARY_ANALYZER_HOST"),
+			FlightsCollectorHost:  os.Getenv("FLIGHTS_COLLECTOR_HOST"),
+		}
+		tmpl, err := template.ParseFiles("components/web/static/index.html")
+		if err != nil {
+			http.Error(w, "Could not load template", http.StatusInternalServerError)
+			return
+		}
+		err = tmpl.Execute(w, config)
+		if err != nil {
+			panic(err)
+		}
+	})
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
