62616812bb0590890bfac9633b8265114e991b4f
prodigeris <arnas.kazlauskas@kilo.health>
2023-10-02 23:21:55 +0300

    fix(database): debug

--- database/migrate.go
+++ database/migrate.go
@@ -43,6 +43,8 @@ func getDB() (*sql.DB, error) {
 		pgHost, pgPort, pgUser, pgPassword, pgDBName, pgSSLMode,
 	)
 
+	fmt.Println("Trying to open", connStr)
+
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return nil, err
